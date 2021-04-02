f79016c2eb5040625e54c9aaeb18d5e91bfbe823
TJ Janus <TJJanus1@gmail.com>
2021-04-02 02:25:32 -0400

    test

--- database/seeds/01-teams.js
+++ database/seeds/01-teams.js
@@ -5,7 +5,7 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('teams').insert([
-        {id: 1, colName: 'rowValue1'},
+        {id: 1, teams: 'rowValue1'},
         {id: 2, colName: 'rowValue2'},
         {id: 3, colName: 'rowValue3'}
       ]);
